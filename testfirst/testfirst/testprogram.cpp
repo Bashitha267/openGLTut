33e08b44a3dc67274e71b003b2a6dcbf3b738aa4
Bashitha <nimeshspc2k17@gmail.com>
2026-02-26 22:22:30 -0800

    added first program

--- testfirst/testfirst/testprogram.cpp
+++ testfirst/testfirst/testprogram.cpp
@@ -5,7 +5,6 @@ void init()
 {                 //r   g     b  0.0-1.0
 	glClearColor (1.0, 0.0, 1.0, 1.0);
 			  //r	g	b  0.0-1.0
-	glColor3f(0.0, 0.0, 0.0); 
 
 	glMatrixMode (GL_PROJECTION);    
 	glLoadIdentity ();    
@@ -61,6 +60,8 @@ void mydisplay3(){
 	glEnd();
 	glFlush(); 
 }
+
+
 void mydisplay4(){
     glClear(GL_COLOR_BUFFER_BIT); 
 	glLineWidth(3.0f);
@@ -73,13 +74,91 @@ void mydisplay4(){
 	glFlush(); 
 }
 
+void mytrianglefan(){
+	glClear(GL_COLOR_BUFFER_BIT); 
+	glLineWidth(3.0f);
+	glBegin(GL_TRIANGLE_FAN); 
+	   
+		
+		glVertex2f(0, 0);
+		glColor3f(1.0,0.0,0.0);
+		glVertex2f(0.9, 0); 
+		glColor3f(1.0,0.0,0.0);
+		glVertex2f(0.6, 0.6);
+		glColor3f(1.0,0.0,0.0);
+
+		
+		glVertex2f(0, 0);
+		glColor3f(0.0,1.0,0.0);
+		glVertex2f(0.6, 0.6);
+		glColor3f(0.0,1.0,0.0);
+		glVertex2f(0,0.9);     
+		glColor3f(0.0,1.0,0.0);
+
+		glVertex2f(0, 0);  
+		glColor3f(0.0,0.0,1.0);
+		glVertex2f(0, 0.9);
+		glColor3f(0.0,0.0,1.0);
+		glVertex2f(-0.6,0.6);    
+		glColor3f(0.0,0.0,1.0);
+
+		glVertex2f(0, 0);
+		glColor3f(0.0,1.0,1.0);
+		glVertex2f(-0.6, 0.6);
+		glColor3f(0.0,1.0,1.0);
+		glVertex2f(-0.9,0);    
+		glColor3f(0.0,1.0,1.0);
+
+		glVertex2f(0, 0); 
+		glColor3f(0,25.0,0.25);
+		glVertex2f(-0.9, 0);        
+				glColor3f(0.25,0.0,0.25);
+
+		glVertex2f(-0.6, -0.6);
+				glColor3f(0.25,0.0,0.25);
+
+
+		glVertex2f(0, 0);  
+		glColor3f(0.0,0.5,1.0);
+		glVertex2f(-0.6, -0.6);
+		glColor3f(0.0,0.5,1.0);
+		glVertex2f(0,-0.9);     
+		glColor3f(0.0,0.5,1.0);
+
+		glVertex2f(0, 0);
+		glColor3f(0.5,0.0,1.0);
+		glVertex2f(0,-0.9);
+		glColor3f(0.5,0.0,1.0);
+		glVertex2f(0.6,-0.6);    
+		glColor3f(0.5,0.0,1.0);
+
+		glVertex2f(0, 0);
+		glColor3f(0.0,0.5,0.5);
+		glVertex2f(0.6, -0.6);
+		glColor3f(0.0,0.5,0.5);
+		glVertex2f(0.9,0);    
+		glColor3f(0.0,0.5,0.5);
+
+		//glVertex2f(-0.6, -0.6);
+		//glVertex2f(0, -0.9);        
+
+		//glVertex2f(0.6, -0.6);
+
+
+
+		//glVertex2f(0.25, -0.75);    
+	glEnd();
+	glFlush(); 
+
+}
+
 int main(int argc, char** argv){
 	glutInit(&argc, argv);
 	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);  
 	glutInitWindowSize(500,500);
 	glutInitWindowPosition(0,0);
 	glutCreateWindow("Test");     
-	glutDisplayFunc(mytriangle);  
+	glutDisplayFunc(mytrianglefan);  
 	init();  
 	glutMainLoop();
 }
